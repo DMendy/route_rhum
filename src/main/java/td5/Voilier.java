9f7edb5ffc3ede6678cc1188292ae201433e0f82
DMendy <dorymendy@icloud.com>
2025-01-27 12:18:04 +0100

    documenation de la classe Voilier

--- src/main/java/td5/Voilier.java
+++ src/main/java/td5/Voilier.java
@@ -1,31 +1,69 @@
 package td5;
 
+/**
+ * Abstract class representing a sailboat.
+ * This class includes the name of the sailboat and a strategy for following a route.
+ */
 public abstract class Voilier {
+    /**
+     * The name of the sailboat.
+     */
     private String nom;
+
+    /**
+     * Strategy for following a route.
+     */
     private SuivreRoute route;
 
+    /**
+     * Constructor with parameters for name and route.
+     *
+     * @param nom the name of the sailboat.
+     * @param route the strategy for following a route.
+     */
     public Voilier(String nom, SuivreRoute route) {
         this.nom = nom;
         this.route = route;
     }
 
-    public Voilier(String nom){
-        this.nom =nom;
+    /**
+     * Constructor with parameter for name only.
+     *
+     * @param nom the name of the sailboat.
+     */
+    public Voilier(String nom) {
+        this.nom = nom;
     }
 
-    public Voilier(){
+    /**
+     * Default constructor.
+     */
+    public Voilier() {
     }
 
-    public void setSuivreRoute(SuivreRoute suivreRoute){
+    /**
+     * Sets the route strategy.
+     *
+     * @param suivreRoute the strategy for following a route.
+     */
+    public void setSuivreRoute(SuivreRoute suivreRoute) {
         this.route = suivreRoute;
     }
 
-    public void appliqueSuivreRoute(){
+    /**
+     * Applies the route strategy.
+     */
+    public void appliqueSuivreRoute() {
         route.suivreRoute();
     }
 
+    /**
+     * Provides a string representation of the sailboat.
+     *
+     * @return a string representation of the sailboat.
+     */
     @Override
     public String toString() {
-        return "Voilier : " + nom ;
+        return "Voilier : " + nom;
     }
 }
